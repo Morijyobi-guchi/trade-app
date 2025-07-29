65a278e082ed599d826b85817e52c523b271a7d2
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2025-07-29 14:31:27 +0900

    元からあったseederを元に戻す

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,41 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
+        // User::factory(10)->create();
 
-        // 一般ユーザーのテストデータを作成
-        User::create([
-            'user_name' => '田中太郎',
-            'mail_address' => 'tanaka@example.com',
-            'department' => 1,
-            'entry_year' => 2022,
-            'password' => bcrypt('password'),
-            'account_expiration_date' => '2026-03-31',
-            'delete_flag' => false,
-        ]);
-
-        User::create([
-            'user_name' => '佐藤花子',
-            'mail_address' => 'sato@example.com',
-            'department' => 2,
-            'entry_year' => 2024,
-            'password' => bcrypt('password'),
-            'account_expiration_date' => '2027-03-31',
-            'delete_flag' => false,
-        ]);
-        User::create([
-            'user_name' => '山田タコ朗',
-            'mail_address' => 'tako@example.com',
-            'department' => 3,
-            'entry_year' => 2024,
-            'password' => bcrypt('password'),
-            'account_expiration_date' => '2026-03-31',
-            'delete_flag' => false,
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
