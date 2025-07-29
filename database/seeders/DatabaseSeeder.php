48cb5e4f1dae21710172348169a12f885f042029
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2025-07-29 14:12:35 +0900

    アカウントテーブルの修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // 複数のテストユーザーを作成
-        User::factory(10)->create();
 
         // 一般ユーザーのテストデータを作成
-        User::factory()->create([
+        User::create([
             'user_name' => '田中太郎',
             'mail_address' => 'tanaka@example.com',
             'department' => 1,
@@ -27,7 +21,7 @@ class DatabaseSeeder extends Seeder
             'delete_flag' => false,
         ]);
 
-        User::factory()->create([
+        User::create([
             'user_name' => '佐藤花子',
             'mail_address' => 'sato@example.com',
             'department' => 2,
@@ -36,7 +30,7 @@ class DatabaseSeeder extends Seeder
             'account_expiration_date' => '2027-03-31',
             'delete_flag' => false,
         ]);
-        User::factory()->create([
+        User::create([
             'user_name' => '山田タコ朗',
             'mail_address' => 'tako@example.com',
             'department' => 3,
