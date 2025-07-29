5da443e74d86b3caafaac27c0aea17f79ad8c4e5
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2025-07-29 14:32:15 +0900

    アカウントseederの作成

---
+++ database/seeders/AccountsSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Accounts;
+use Illuminate\Database\Seeder;
+
+class AccountsSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+        // 一般ユーザーのテストデータを作成
+        Accounts::create([
+            'user_name' => '田中太郎',
+            'mail_address' => 'tanaka@example.com',
+            'department' => 1,
+            'entry_year' => 2022,
+            'password' => bcrypt('password'),
+            'account_expiration_date' => '2026-03-31',
+            'delete_flag' => false,
+        ]);
+
+        Accounts::create([
+            'user_name' => '佐藤花子',
+            'mail_address' => 'sato@example.com',
+            'department' => 2,
+            'entry_year' => 2024,
+            'password' => bcrypt('password'),
+            'account_expiration_date' => '2027-03-31',
+            'delete_flag' => false,
+        ]);
+        Accounts::create([
+            'user_name' => '山田タコ朗',
+            'mail_address' => 'tako@example.com',
+            'department' => 3,
+            'entry_year' => 2024,
+            'password' => bcrypt('password'),
+            'account_expiration_date' => '2026-03-31',
+            'delete_flag' => false,
+        ]);
+    }
+}
+
