f10ecdda77aa8f7813b80b093cff16accbef2c5c
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2025-08-04 16:43:31 +0900

    urlの追加

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\matchController;
+use App\Http\Controllers\detailGoodsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/match-tags', [matchController::class, 'matchTags']);
+Route::get('/goods-detail', [detailGoodsController::class, 'detail']);
