ab95e93bee096c1a64da76f990dcb1f04198701c
mukaiguti <y.mukaiguchi.sys24@morijyobi.ac.jp>
2025-08-05 15:58:48 +0900

    top画面の作成

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\matchController;
+use App\Http\Controllers\topController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/top', function () {
+    return view('top');
+});
+
 Route::get('/match-tags', [matchController::class, 'matchTags']);
+
+Route::get('/top', [topController::class, 'goods']);
+
